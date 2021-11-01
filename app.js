255b760b259e68a902c577c39cf0aedede6f53b5
Ido Strauss <strauss02@hotmail.co.il>
2021-11-01 16:50:18 +0200

    deployment

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = new express()
+
+const cors = require('cors')
+const path = require('path')
+
+app.use(cors()) // cors middleware
+
+app.use('/', express.static(path.resolve('./dist'))) // serve main path as static dir
+app.get('/', function (req, res) {
+  // serve main path as static file
+  res.sendFile(path.resolve('./dist/index.html'))
+})
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
