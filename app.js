110547a8b6245abe3e145795d150d9d5ec443562
Ido Strauss <strauss02@hotmail.co.il>
2021-11-01 17:22:47 +0200

    fix path

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 const userRouter = require('./src/routers/userRouter')
 const userHandler = require('./src/middleware/userHandler')
-const pokemonRouter = require('./routers/pokemonRouter')
+const pokemonRouter = require('./src/routers/pokemonRouter')
 const errorHandler = require('./src/middleware/errorHandler')
 const Pokedex = require('pokedex-promise-v2')
 const P = new Pokedex()
